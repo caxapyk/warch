93878ebedb327189f62ff1b0b166fb89a5fe8d71
caxapyk <saharuk.alexander@gmail.com>
2020-12-12 09:11:35 +0300

    wsgi import after venv activate

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
 import os, sys
-from werkzeug.exceptions import NotFound
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.append(basedir)
-
 activate_this = "%s/warch/venv/bin/activate_this.py" % basedir
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
+sys.path.append(basedir)
 sys.path.append(basedir + '/warch/apps')
+
+from werkzeug.exceptions import NotFound
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from warch_decision import app as decision_app
 from warch_registry import app as registry_app
