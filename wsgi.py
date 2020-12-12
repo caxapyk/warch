b542df5fe87a90d04d00b945af1cf4088409413b
caxapyk <saharuk.alexander@gmail.com>
2020-12-12 08:59:59 +0300

    warch shared virtaul enviroment

--- wsgi.py
+++ wsgi.py
@@ -4,12 +4,17 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.append(basedir)
+
+activate_this = "%s/warch/venv/bin/activate_this.py" % basedir
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
 sys.path.append(basedir + '/warch/apps')
 
-from warch.apps.warch_decision.wsgi import application as decision_app
-from warch.apps.warch_registry.wsgi import application as registry_app
+from warch_decision import app as decision_app
+from warch_registry import app as registry_app
 
 application = DispatcherMiddleware(NotFound(), {
-    '/_decision': decision_app,
-    '/_registry': registry_app
+    '/_decision': decision_app.create_app(),
+    '/_registry': registry_app.create_app()
 })
