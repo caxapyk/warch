0e66cfe505dc77125eda333209f4d7d42cae7c38
Sakharuk Alexander <saharuk.alexander@gmail.com>
2021-03-10 13:13:29 +0300

    staticstics on index page, report page, decision on update noew can clear protocol

--- server.py
+++ server.py
@@ -2,6 +2,5 @@ from livereload import Server
 from wsgi import application
 
 
-if __name__ == '__main__':
-    server = Server(application)
-    server.serve(host='localhost', port=5000)
+server = Server(application)
+server.serve(host='localhost', port=5000)
